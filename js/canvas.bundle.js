bcd3e4963ec06fabe1a8e6eb6dd7b074037ae421
Ferdinand Saurenbach <ferdinand161@outlook.de>
2022-04-30 09:21:37 +0200

    Add files via upload

---
+++ js/canvas.bundle.js
@@ -0,0 +1,416 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/canvas.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/img/background.png":
+/*!********************************!*\
+  !*** ./src/img/background.png ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "072d51bcc9c09311d4c2a6708b05bddc.png");
+
+/***/ }),
+
+/***/ "./src/img/hills.png":
+/*!***************************!*\
+  !*** ./src/img/hills.png ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "cfffe4c371f5e11d372b398a87c51dd0.png");
+
+/***/ }),
+
+/***/ "./src/img/platform.png":
+/*!******************************!*\
+  !*** ./src/img/platform.png ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "ffab39d3487de561be1a081fcfb3806d.png");
+
+/***/ }),
+
+/***/ "./src/img/player.png":
+/*!****************************!*\
+  !*** ./src/img/player.png ***!
+  \****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "bf39c4c20bcd4951d5c2aa50f55e844f.png");
+
+/***/ }),
+
+/***/ "./src/js/canvas.js":
+/*!**************************!*\
+  !*** ./src/js/canvas.js ***!
+  \**************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _img_background_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../img/background.png */ "./src/img/background.png");
+/* harmony import */ var _img_hills_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../img/hills.png */ "./src/img/hills.png");
+/* harmony import */ var _img_platform_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../img/platform.png */ "./src/img/platform.png");
+/* harmony import */ var _img_player_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../img/player.png */ "./src/img/player.png");
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var canvas = document.querySelector('canvas');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+var space_pressed = 0;
+var c = canvas.getContext('2d');
+var gravity = 5;
+
+
+
+
+
+function createImage(imageSrc) {
+  var image = new Image();
+  image.src = imageSrc;
+  return image;
+}
+
+var platformImage = createImage(_img_platform_png__WEBPACK_IMPORTED_MODULE_2__["default"]);
+var playerImage = createImage(_img_player_png__WEBPACK_IMPORTED_MODULE_3__["default"]);
+
+var Player = /*#__PURE__*/function () {
+  function Player() {
+    _classCallCheck(this, Player);
+
+    this.position = {
+      x: 100,
+      y: 100
+    };
+    this.velocity = {
+      x: 0,
+      y: 0
+    };
+    this.width = 62;
+    this.height = 62;
+    this.speed = 10;
+  }
+
+  _createClass(Player, [{
+    key: "draw",
+    value: function draw() {
+      c.drawImage(playerImage, this.position.x, this.position.y);
+    }
+  }, {
+    key: "collidesWith",
+    value: function collidesWith(player, platform) {
+      platform.forEach(function (platform) {
+        if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+          console.log('Player collidesWith platform');
+        }
+      });
+    }
+  }, {
+    key: "update",
+    value: function update() {
+      this.draw();
+      this.position.y += this.velocity.y;
+      this.position.x += this.velocity.x;
+      if (this.position.y + this.height + this.velocity.y <= canvas.height) this.velocity.y += gravity;
+    }
+  }]);
+
+  return Player;
+}();
+
+var Platform = /*#__PURE__*/function () {
+  function Platform(x, y, image) {
+    _classCallCheck(this, Platform);
+
+    this.position = {
+      x: x,
+      y: y
+    };
+    this.image = image;
+    this.width = this.image.width;
+    this.height = this.image.height;
+  }
+
+  _createClass(Platform, [{
+    key: "draw",
+    value: function draw() {
+      c.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }]);
+
+  return Platform;
+}();
+
+var GenericObject = /*#__PURE__*/function () {
+  function GenericObject(x, y, image) {
+    _classCallCheck(this, GenericObject);
+
+    this.position = {
+      x: x,
+      y: y
+    };
+    this.image = image;
+    this.width = this.image.width;
+    this.height = this.image.height;
+  }
+
+  _createClass(GenericObject, [{
+    key: "draw",
+    value: function draw() {
+      c.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }]);
+
+  return GenericObject;
+}();
+
+var player = new Player();
+var platforms = [new Platform(-1, 660, platformImage), new Platform(platformImage.width - 3, 660, platformImage), new Platform(platformImage.width * 2 + 100, 660, platformImage)];
+var genericObjects = [new GenericObject(-1, -1, createImage(_img_background_png__WEBPACK_IMPORTED_MODULE_0__["default"])), new GenericObject(-1, -1, createImage(_img_hills_png__WEBPACK_IMPORTED_MODULE_1__["default"]))];
+var scrollOffset = 0;
+
+function init() {
+  console.log('init');
+  player = new Player();
+  platforms = [new Platform(0, 660 - 50, platformImage), new Platform(100, 660 - 50, platformImage), new Platform(200, 660 - 50, platformImage), new Platform(400, 510 - 50, platformImage), new Platform(500, 510 - 50, platformImage), new Platform(600, 510 - 50, platformImage), new Platform(700, 510 - 50, platformImage), new Platform(975, 510 - 50, platformImage), new Platform(1075, 510 - 50, platformImage), new Platform(1175, 510 - 50, platformImage), new Platform(1275, 510 - 50, platformImage), new Platform(1575, 660 - 50, platformImage), new Platform(1675, 660 - 50, platformImage), new Platform(1775, 660 - 50, platformImage)];
+  genericObjects = [new GenericObject(-1, -1, createImage(_img_background_png__WEBPACK_IMPORTED_MODULE_0__["default"])), new GenericObject(-1, -1, createImage(_img_hills_png__WEBPACK_IMPORTED_MODULE_1__["default"]))];
+  scrollOffset = 0;
+}
+
+var keys = {
+  right: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  }
+};
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.fillStyle = 'black';
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  genericObjects.forEach(function (genericObjects) {
+    genericObjects.draw();
+  });
+  platforms.forEach(function (platform) {
+    platform.draw();
+  });
+  player.update();
+
+  if (first_time) {
+    init();
+    first_time = false;
+  }
+
+  if (keys.right.pressed && player.position.x < 400) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed && player.position.x > 100 || keys.left.pressed && scrollOffset === 0 && player.position.x > 50) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+
+    if (keys.right.pressed) {
+      scrollOffset += player.speed;
+      platforms.forEach(function (platform) {
+        platform.position.x -= player.speed;
+      });
+      genericObjects.forEach(function (genericObject) {
+        genericObject.position.x -= player.speed * .66;
+      });
+    } else if (keys.left.pressed && scrollOffset > 4) {
+      console.log(scrollOffset);
+      scrollOffset -= player.speed * .66;
+      platforms.forEach(function (platform) {
+        platform.position.x += player.speed;
+      });
+      genericObjects.forEach(function (genericObject) {
+        genericObject.position.x += player.speed * .66;
+      });
+    }
+  }
+
+  console.log(player.collidesWith(player, platforms));
+  platforms.forEach(function (platform) {
+    if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+      player.velocity.y = 0;
+      space_pressed = 0;
+    }
+  });
+
+  if (scrollOffset > 400 && won == false) {
+    console.log("You beat the Game!!!");
+    won = true;
+  }
+
+  if (player.position.y > 600) {
+    won = false;
+    console.log(won);
+    init();
+  }
+}
+
+animate();
+var first_time = true;
+
+if (first_time) {
+  init();
+  first_time = false;
+}
+
+addEventListener("keydown", function (event) {
+  KeyDown(event.key);
+});
+addEventListener("keyup", function (event) {
+  KeyUp(event.key);
+});
+
+function KeyUp(key) {
+  switch (key) {
+    case "a":
+      console.log("a");
+      keys.left.pressed = false;
+      break;
+
+    case "d":
+      console.log("d");
+      keys.right.pressed = false;
+      break;
+
+    case " ":
+      console.log("Space");
+      player.velocity.y = 0;
+      break;
+  }
+}
+
+function KeyDown(key) {
+  switch (key) {
+    case "a":
+      console.log("a");
+      keys.left.pressed = true;
+      break;
+
+    case "d":
+      console.log("d");
+      keys.right.pressed = true;
+      break;
+
+    case " ":
+      if (space_pressed < 1) {
+        console.log("Space");
+        player.velocity.y -= 50;
+        space_pressed += 1;
+        break;
+      }
+
+  }
+}
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=canvas.bundle.js.map
